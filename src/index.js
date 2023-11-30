2f3e4ef2af0c74ce4bf7742c8417ca93c83b4bf4
bnoora <bnooranii@gmail.com>
2023-11-30 12:45:13 -0800

    index finished

--- src/index.js
+++ src/index.js
@@ -4,3 +4,29 @@ import home from './home';
 import nav from './nav';
 
 const content = document.getElementById('content');
+
+content.appendChild(nav());
+content.appendChild(home());
+
+const homebtn = document.getElementById('homebtn');
+const menubtn = document.getElementById('menubtn');
+const contactbtn = document.getElementById('contactbtn');
+
+homebtn.addEventListener('click', () => {
+    content.removeChild(content.lastChild);
+    content.appendChild(home());
+}
+);
+
+menubtn.addEventListener('click', () => {
+    content.removeChild(content.lastChild);
+    content.appendChild(menu());
+}
+);
+
+contactbtn.addEventListener('click', () => {
+    content.removeChild(content.lastChild);
+    content.appendChild(contact());
+}
+);
+
